f1cdfbe9ff9747b1a9458b20d832943c4e0d5dcf
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-11 16:40:37 +0900

    📰 New 로그인 페이지 생성

---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+/**
+File Name : Login
+Description : 로그인 페이지
+Author : 임지영
+
+History
+Date        Author   Status    Description
+2024.06.10  임지영   Created
+*/
+
+import React from 'react'
+import Header from '../components/common/Header'
+
+const Login = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Login
