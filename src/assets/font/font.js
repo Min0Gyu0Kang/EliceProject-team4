0e77cb0360c3cfe56e08fd36bf4c9cf6862e9e9c
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-11 11:12:54 +0900

    🔄 Rename 이미지 확장자 png -> svg

---
+++ src/assets/font/font.js
@@ -0,0 +1,13 @@
+// GlobalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const LogoFont = createGlobalStyle`
+  @font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
+export default LogoFont;
