a9486a66066954d176ebde62686fc3ce1c8a892a
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-11 16:40:15 +0900

    📝 Comment description 주석 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+/**
+File Name : index
+Description : 렌더링 담당 페이지
+Author : 임지영
+
+History
+Date        Author   Status    Description
+2024.06.08  임지영   Created
+2024.06.11  임지영   Modified    설명 주석 추가
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './pages/App.js'
-import BarChart from './components/BarChart.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
