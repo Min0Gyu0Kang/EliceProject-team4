5040bb1223160f664ebae884f79eb92721b59a07
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-11 11:14:00 +0900

    🔀 Restructure 폴더 생성 및 파일 경로 변경

---
+++ src/pages/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Map = () => {
+  return (
+    <div>
+      <Header />
+      안녕하세요
+    </div>
+  );
+};
+
+export default Map;
