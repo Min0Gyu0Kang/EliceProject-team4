cbd044297bcc475e982cb5f13a12cf3ab90ae597
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-11 16:38:07 +0900

    ✨ Feat Footer 적용 잘 되는지 확인

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,23 @@
+/**
+File Name : Map
+Description : 지도 페이지 (기능 확인용)
+Author : 임지영
+
+History
+Date        Author   Status    Description
+2024.06.10  임지영   Created
+*/
+
 import React from 'react'
-import Header from '../components/Header'
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
 
 const Map = () => {
     return (
         <div>
             <Header />
             안녕하세요
+            <Footer />
         </div>
     )
 }
