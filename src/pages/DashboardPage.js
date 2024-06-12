7c3befade5b132b4da1739030a21bd36ff085cff
yurim <kinyou9894@naver.com>
2024-06-12 12:02:18 +0900

    📝 Comment: 주석 추가

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import Header from '../components/header/Header'
-import DashBoard from '../components/dashboard/DashBoard' // 수정된 부분
-import Navigation from '../components/dashboard/Navigation'
+/**
+File Name : DashboardPage
+Description : 대시보드 페이지
+Author : 김유림
+
+History
+Date        Author   Status    Description
+2024.06.10  김유림   Created
+*/
+
+import React from 'react';
+import Header from '../components/common/Header';
+import DashBoard from '../components/dashboard/DashBoard';
+import Navigation from '../components/dashboard/Navigation';
+import Footer from '../components/common/Footer';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    position: relative; /* 추가 */
+`;
+
+const ContentWrapper = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+`;
 
 const DashboardPage = () => {
     return (
-        <div>
+        <PageContainer>
             <Header />
             <Navigation />
-            <DashBoard />
-        </div>
-    )
-}
+            <ContentWrapper>
+                <DashBoard />
+            </ContentWrapper>
+            <Footer />
+        </PageContainer>
+    );
+};
 
-export default DashboardPage
+export default DashboardPage;
