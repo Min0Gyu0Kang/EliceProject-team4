00710cda3bea53c29ccf83ff37363e2106fee49d
AI 10기 강민규 <myname@g.skku.edu>
2024-06-13 09:39:32 +0900

    �🎨 Style App.js 히스토리

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ Date        Author   Status    Description
 2024.06.11  강민규   Modified   지도 찾기 분리
 2024.06.11  강민규   Modified   헤더 Router 추가
 2024.06.11  강민규   Modified   결과 / 리뷰 분리
+2024.06.13  강민규   Modified   지도 (임시) svg 추가
 
 */
 
