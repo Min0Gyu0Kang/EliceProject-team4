59d4f7598bb737e0b663a222ec7a207d238cbc48
AI 10기 강민규 <myname@g.skku.edu>
2024-06-13 15:22:52 +0900

    ✨ Feat Map.js 줌 기능 마커(임시)

--- src/services/Map.js
+++ src/services/Map.js
@@ -7,20 +7,78 @@ History
 Date        Author   Status    Description
 2024.06.10  강민규   Created
 2024.06.10  강민규   Modified   
-2024.06.10  강민규   Modified  지도 시각화(초안)  
+2024.06.13  강민규   Modified  지도 시각화(초안)  
+2024.06.13  강민규   Modified  지도 줌인아웃  
+2024.06.13  강민규   Modified  지도 임시위치 마커  
 */
 
 import * as React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Map = () => {
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
+    useEffect(() => {
+        const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
+        const options = { // 지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
+            level: 3 // 지도의 레벨(확대, 축소 정도)
+        };
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
+        // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+        const mapTypeControl = new kakao.maps.MapTypeControl();
+
+        // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+        // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+        const zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+        // 마커를 표시할 위치와 title 객체 배열입니다 
+        var positions = [
+            {
+                title: '카카오', 
+                latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+            },
+            {
+                title: '생태연못', 
+                latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+            },
+            {
+                title: '텃밭', 
+                latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+            },
+            {
+                title: '근린공원',
+                latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+            }
+        ];
+
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+            
+        for (var i = 0; i < positions.length; i ++) {
+            
+            // 마커 이미지의 이미지 크기 입니다
+            var imageSize = new kakao.maps.Size(24, 35); 
+            
+            // 마커 이미지를 생성합니다    
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+            
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                image : markerImage // 마커 이미지 
+            });
+        }  
+    }, []);
+
+    
 
     return (
         <div id="map" style={{ width: '500px', height: '400px' }}></div>
