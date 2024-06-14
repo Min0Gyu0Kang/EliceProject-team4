4c0d353f9d406f621015621373b418caa87ba521
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-14 16:17:12 +0900

    📰 New 빈탭문구

---
+++ src/components/common/Empty.js
@@ -0,0 +1,37 @@
+/**
+File Name : Empty
+Description : 빈탭 문구 컴포넌트
+Author : 임지영
+
+History
+Date        Author   Status    Description
+2024.06.14  임지영   Created
+*/
+
+import React from 'react'
+import styled from 'styled-components'
+import '../../assets/fonts/font.css'
+
+const EmptyTextContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`
+
+const EmptyText = styled.div`
+    text-align: center;
+    opacity: 0.5;
+    font-size: 12pt;
+    font-family: 'Pretendard';
+`
+
+const Empty = ({text}) => {
+    return (
+        <EmptyTextContainer>
+            <EmptyText>{text}</EmptyText>
+        </EmptyTextContainer>
+    )
+}
+
+export default Empty
