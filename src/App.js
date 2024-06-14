ff582029037a7cfc5bdb53147b94f9bb80751ba1
yurim <kinyou9894@naver.com>
2024-06-14 23:02:47 +0900

    ✨ Feat: 커뮤니티 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/DashboardPage'
+import Community from './pages/CommunityPage'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/dataStory" element={<Dashboard />} />
+                <Route path="/community" element={<Community />} />
             </Routes>
         </BrowserRouter>
     )
