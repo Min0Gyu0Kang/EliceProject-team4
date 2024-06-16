283eaa9f6307fb64af60df82ce19cd46fd4c009c
yurim <kinyou9894@naver.com>
2024-06-16 23:00:22 +0900

    💄 Design: height고정

--- src/components/map/output/InfoList.js
+++ src/components/map/output/InfoList.js
@@ -8,9 +8,9 @@ Date        Author   Status    Description
 2024.06.14  김유림    Created   공원 정보 리스트 컴포넌트
 2024.06.15  임지영    Modified  리스트 컨테이너 높이 수정
 2024.06.15  김유림    Modified  리스트 컨테이너 높이 수정
+2024.06.16  김유림    Modified  park 받아와서 공원정보 리스트 나열 높이 고정
 */
-
-import React, {useState} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import {StyledEngineProvider} from '@mui/styled-engine'
 import List from '@mui/material/List'
@@ -21,7 +21,6 @@ import LocationIcon from '../../../assets/images/location.svg'
 import DivisionIcon from '../../../assets/images/division.svg'
 import TelIcon from '../../../assets/images/telephone.svg'
 import InformationIcon from '../../../assets/images/information.svg'
-import {useInfoData} from '../../common/useInfoData'
 
 const Container = styled.div`
     display: flex;
@@ -32,7 +31,7 @@ const Container = styled.div`
 const InfoListContainer = styled.div`
     width: 100%;
     max-width: 370px;
-    max-height: 330px; /* 리스트 높이 제한 설정 */
+    height: 330px; /* 높이 고정 */
     border-radius: 16px;
     background-color: #ffffff; /* 배경색 설정 */
     border: 1px solid #e0e0e0; /* 테두리 설정 */
@@ -58,66 +57,50 @@ const StyledList = styled(List)`
     padding: 0;
 `
 
-const InfoList = () => {
-    const [url, setUrl] = useState('')
-    const {infoData, error} = useInfoData(url)
+const InfoItem = ({icon, primary, secondary}) => (
+    <ListItem sx={{paddingBottom: 0}}>
+        <ListItemAvatar>
+            <img src={icon} alt={`${primary} Icon`} />
+        </ListItemAvatar>
+        <ListItemText primary={primary} secondary={secondary} />
+    </ListItem>
+)
 
+const InfoList = ({park}) => {
     return (
         <StyledEngineProvider injectFirst>
             <Container>
                 <InfoListContainer>
                     <StyledList>
-                        <ListItem sx={{paddingBottom: 0}}>
-                            <ListItemAvatar>
-                                <img src={LocationIcon} alt="Location Icon" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="주소"
-                                secondary="서울특별시 강남구 대치동 501"
-                            />
-                        </ListItem>
-                        <ListItem sx={{paddingBottom: 0}}>
-                            <ListItemAvatar>
-                                <img src={DivisionIcon} alt="Division Icon" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="공원구분"
-                                secondary="어린이공원"
-                            />
-                        </ListItem>
-                        <ListItem sx={{paddingBottom: 0}}>
-                            <ListItemAvatar>
-                                <img src={TelIcon} alt="Tel Icon" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="전화번호"
-                                secondary="02-1234-1234"
-                            />
-                        </ListItem>
-                        <ListItem sx={{paddingBottom: 0}}>
-                            <ListItemAvatar>
-                                <img
-                                    src={InformationIcon}
-                                    alt="Information Icon"
-                                />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="운동시설"
-                                secondary="농구장, 축구장, 테니스장"
-                            />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <img
-                                    src={InformationIcon}
-                                    alt="Information Icon"
-                                />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="편익시설"
-                                secondary="블라블라"
-                            />
-                        </ListItem>
+                        <InfoItem
+                            icon={LocationIcon}
+                            primary="주소"
+                            secondary={park.address}
+                        />
+                        <InfoItem
+                            icon={DivisionIcon}
+                            primary="공원구분"
+                            secondary={park.type}
+                        />
+                        <InfoItem
+                            icon={TelIcon}
+                            primary="전화번호"
+                            secondary={park.phone}
+                        />
+                        {/* 운동 및 편익 시설을 리스트로 표시 */}
+                        {park.facilities && (
+                            <>
+                                {park.facilities &&
+                                    park.facilities.map((facility, index) => (
+                                        <InfoItem
+                                            key={`facility-${index}`}
+                                            icon={InformationIcon}
+                                            primary={facility.category}
+                                            secondary={facility.name}
+                                        />
+                                    ))}
+                            </>
+                        )}
                     </StyledList>
                 </InfoListContainer>
             </Container>
