39ae6a535774e95ae002f43edb7132577167d1ef
[AI10] 임지영 <dlawldud021@naver.com>
2024-06-18 20:18:23 +0900

    none

--- src/config.js
+++ src/config.js
@@ -8,16 +8,14 @@ Date        Author   Status    Description
 2024.06.17  임지영    Created   
 */
 
-const BASE_URL = 'http://localhost:4000'
-
 const API = {
-    dashboard: `${BASE_URL}/dashboard`,
-    map: `${BASE_URL}/map`,
-    park_sido: `${BASE_URL}/recommend/city`,
-    recommend_park: `${BASE_URL}/park/recommend`,
-    search_park: `${BASE_URL}/park/search`,
-    park_info: `${BASE_URL}/park/infomation`,
-    park_review: `${BASE_URL}/park-review`,
-    show_park_review: `${BASE_URL}/park-review/details`,
+    dashboard: `/dashboard`,
+    map: `/map`,
+    park_sido: `/recommend/city`,
+    recommend_park: `/park/recommend`,
+    search_park: `/park/search`,
+    park_info: `/park/infomation`,
+    park_review: `/park-review`,
+    show_park_review: `/park-review/details`,
 }
 export default API
