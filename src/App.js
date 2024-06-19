3a4f8f33b6de081036905e0af78d22493bf0bd0d
yurim <kinyou9894@naver.com>
2024-06-19 16:38:29 +0900

    ✨ Feat: 헤더 로그인 시 상태 변화

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ History
 Date        Author   Status    Description
 2024.06.10  임지영   Created
 2024.06.11  임지영   Modified    로그인 경로 추가
+2024.06.19  김유림   Modified    헤더 로그인/로그아웃 시 변경
 */
 
-import React from 'react'
+import React, {useState} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/common/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
@@ -18,11 +20,26 @@ import Dashboard from './pages/DashboardPage'
 import Community from './pages/CommunityPage'
 
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+    }
+
+    const handleLogout = () => {
+        setIsLoggedIn(false)
+    }
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+            <Routes>
+                <Route
+                    path="/login"
+                    element={<Login onLogin={handleLogin} />}
+                />
                 <Route path="/signUp" element={<SignUp />} />
                 <Route path="/dataStory" element={<Dashboard />} />
                 <Route path="/community" element={<Community />} />
