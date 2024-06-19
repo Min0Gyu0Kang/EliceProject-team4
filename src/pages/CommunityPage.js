1b5d4e82858ea6bee7d4642b7833d2f0d5b642df
yurim <kinyou9894@naver.com>
2024-06-19 16:41:21 +0900

    💄 Design: 헤더 제거

--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -6,10 +6,10 @@ Author : 김유림
 History
 Date        Author   Status    Description
 2024.06.10  김유림     Created
+2024.06.19  김유림     Modified  헤더 삭제 app.js에서 관리
 */
 
 import React from 'react'
-import Header from '../components/common/Header'
 import Navigation from '../components/common/Navigation'
 import communityIcon from '../assets/images/community.svg'
 import Footer from '../components/common/Footer'
@@ -31,7 +31,6 @@ const ContentWrapper = styled.div`
 const DashboardPage = () => {
     return (
         <PageContainer>
-            <Header />
             <Navigation
                 title="커뮤니티"
                 subtitle="공원에 대해 다양한 소통을 해보세요"
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -8,11 +8,12 @@ Date        Author   Status    Description
 2024.06.10  김유림     Created
 2024.06.13  김유림     Modified  네비게이션 컴포넌트 재사용하도록 수정 
 2024.06.14  김유림     Modified  웹폰트 적용
+2024.06.18  이유민     Modified  DashBoard 경로 수정
+2024.06.19  김유림     Modified  헤더 삭제 app.js에서 관리
 */
 
 import React from 'react'
-import Header from '../components/common/Header'
-import DashBoard from '../components/dashBoard/DashBoard'
+import DashBoard from '../components/dashboard/DashBoard'
 import Navigation from '../components/common/Navigation'
 import goalIcon from '../assets/images/goal.svg'
 import Footer from '../components/common/Footer'
@@ -35,7 +36,6 @@ const ContentWrapper = styled.div`
 const DashboardPage = () => {
     return (
         <div>
-            <Header />
             <PageContainer>
                 <Navigation
                     title="데이터 스토리"
