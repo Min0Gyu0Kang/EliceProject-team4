c95b6da0c213d53d1cb86366b9b397530fcfb72b
youmin29 <babybana0829@naver.com>
2024-06-20 20:25:29 +0900

    ✨ Feat: 지도 수정

--- src/components/map/main/Map.js
+++ src/components/map/main/Map.js
@@ -14,25 +14,99 @@ Date        Author   Status    Description
 2024.06.17  강민규   Modified  지도 클러스터 없음 
 2024.06.19  강민규   Modified  지도 백엔드 /park/recommend 성공
 2024.06.19  강민규   Modified  지도 Query 불러서 마커 반영 성공    
+2024.06.20  이유민   Modified  지도 수정   
 */
 
-// import {React,useEffect,useState} from "react";
-// import styled from "styled-components";
-// import axios from 'axios';
-// // import { city, district, facilities} from "./Query"
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
+const Map = () => {
+    const [parkData, setParkData] = useState([])
 
-// const Map = () => {
-//     var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-//     var options = { //지도를 생성할 때 필요한 기본 옵션
-//         center: new kakao.maps.LatLng(37.566851234596804, 126.97866357016943), //지도의 중심좌표.
-//         level: 3 //지도의 레벨(확대, 축소 정도)
-//     };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    'http://localhost:4000/park/recommend',
+                    {
+                        params: {
+                            city: '강원특별자치도',
+                            district: '고성군',
+                            facilities: '',
+                        },
+                    },
+                )
+                return response.data.data // 데이터 반환
+            } catch (error) {
+                console.error('Error fetching data:', error)
+                return null // 에러 발생 시 null 반환
+            }
+        }
 
-//     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        const getData = async () => {
+            const data = await fetchData()
+            setParkData(data)
+        }
 
-//     return (
-//         <div id="map" style={{ width: '40%', height: '50%' }}></div>
-//     );
-// }
-// export default Map;
+        getData() // 비동기 데이터를 처리하기 위한 함수를 호출
+    }, [])
+
+    useEffect(() => {
+        // 카카오 객체가 로드된 후에 실행되도록 함
+        const script = document.createElement('script')
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=941505440b119603799632887773298c&autoload=false`
+        document.head.appendChild(script)
+
+        script.onload = () => {
+            window.kakao.maps.load(() => {
+                const container = document.getElementById('map')
+                // latitude와 longitude의 평균 계산
+                const avgLat =
+                    parkData.length > 0
+                        ? parkData.reduce(
+                              (sum, park) => sum + park.latitude,
+                              0,
+                          ) / parkData.length
+                        : 37.715133
+                const avgLng =
+                    parkData.length > 0
+                        ? parkData.reduce(
+                              (sum, park) => sum + park.longitude,
+                              0,
+                          ) / parkData.length
+                        : 126.734086
+
+                const options = {
+                    center: new window.kakao.maps.LatLng(avgLat, avgLng),
+                    level: 8,
+                }
+
+                const map = new window.kakao.maps.Map(container, options)
+
+                parkData.forEach(park => {
+                    const markerPosition = new window.kakao.maps.LatLng(
+                        park.latitude,
+                        park.longitude,
+                    )
+                    const marker = new window.kakao.maps.Marker({
+                        position: markerPosition,
+                    })
+                    marker.setMap(map)
+
+                    // 마커 클릭 이벤트 추가
+                    window.kakao.maps.event.addListener(marker, 'click', () => {
+                        console.log(park.id)
+                    })
+                })
+            })
+        }
+
+        return () => {
+            document.head.removeChild(script)
+        }
+    }, [parkData])
+
+    return <div id="map" style={{width: '100%', height: '100%'}}></div>
+}
+
+export default Map
