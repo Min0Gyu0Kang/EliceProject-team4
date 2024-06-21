e2d759d612b68f60bec9870f700296e3a1bf1068
[A10] 박수정 <d-aoo@naver.com>
2024-06-21 10:51:51 +0900

    ✨ Feat: 회원 페이지 추가

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ Description : 라우터
 Author : 임지영
 
 History
-Date        Author   Status    Description
+Date        Author   Status      Description
 2024.06.10  임지영   Created
 2024.06.11  임지영   Modified    로그인 경로 추가
 2024.06.19  김유림   Modified    헤더 로그인/로그아웃 시 변경
+2024.06.20  박수정   Modified    회원 페이지, 마이페이지, 회원정보 수정, 회원탈퇴 기능 추가
 */
 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/common/Header'
 import Home from './pages/Home'
@@ -20,9 +21,21 @@ import Dashboard from './pages/DashboardPage'
 import Community from './pages/CommunityPage'
 import Map from './pages/MapPage'
 // import FindPassword from './pages/FindPassword'
+import UserPage from './pages/UserPage'
+import Mypage from './pages/Mypage'
+import UpdateUser from './pages/UpdateUser'
+import Withdraw from './pages/Withdraw'
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        // RefreshToken 토큰 설정 (테스트)
+        localStorage.setItem(
+            'refreshToken',
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTg5MzI5MjMsImV4cCI6MTcxOTE5MjEyM30.5obJoNwrKBcdat2LQygoDs_AHal7I7ovRpv0RoEeGcQ',
+        )
+    }, [])
 
     const handleLogin = () => {
         setIsLoggedIn(true)
@@ -31,6 +44,7 @@ const App = () => {
     const handleLogout = () => {
         setIsLoggedIn(false)
     }
+
     return (
         <BrowserRouter>
             <Routes>
@@ -47,6 +61,10 @@ const App = () => {
                 <Route path="/dataStory" element={<Dashboard />} />
                 <Route path="/community" element={<Community />} />
                 <Route path="/map" element={<Map />} />
+                <Route path="/users/userpage" element={<UserPage />} />
+                <Route path="/users/mypage" element={<Mypage />} />
+                <Route path="/users/update" element={<UpdateUser />} />
+                <Route path="/users/withdraw" element={<Withdraw />} />
             </Routes>
         </BrowserRouter>
     )
